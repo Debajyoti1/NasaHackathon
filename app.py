13fddcd6bc9dce0e621f8788c73c74ed78470a4b
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-25 23:54:51 +0530

    basic structure complete

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, request, session, redirect, g
-
+from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
+from random import sample
 app = Flask(__name__)
 app.secret_key='Hellothere'
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/data')
+def data():
+    return jsonify({'results' : sample(range(1,50), 7)})
 if __name__ == '__main__':
     app.run(debug=True)

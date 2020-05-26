69a63e34e9887163cfe02d16f1ff05b6b7714763
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-27 00:31:59 +0530

    added population vs confirmed cases graph in index

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask, render_template, url_for, request, session, redirect, g, jsonify
 from random import sample
+import pandas as pd
+
+dataframe=pd.read_csv("dataset.csv")
+confirmed=list(dataframe['Confirmed'])
+population=list(dataframe['Population'])
+area=list(dataframe['Area'])
+density=list(dataframe['Density'])
+active=list(dataframe['Active'])
+
+
 app = Flask(__name__)
 app.secret_key='Hellothere'
+
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/data')
-def data():
-    return jsonify({'results' : sample(range(1,50), 7)})
+
+@app.route('/confirmedj')
+def confirmedj():
+    return jsonify({'confirmed' : confirmed})
+
+
+@app.route('/populationj')
+def populationj():
+    return jsonify({'population' : population})
+
+@app.route('/areaj')
+def areaj():
+    return jsonify({'area' : area})
+
+@app.route('/densityj')
+def densityj():
+    return jsonify({'density' : density})
+
+@app.route('/activej')
+def activej():
+    return jsonify({'active' : active})
+
 if __name__ == '__main__':
     app.run(debug=True)
